8a97a46a17f6aa1bf47640ca793c5d8850e04e6d
Likithsreenivasa <118590713+Likithsreenivasa@users.noreply.github.com>
2023-07-15 08:43:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+let x=prompt('Enter a string')
+let charCount={}
+let res=null
+for(let i=0;i<x.length;i++)
+	{
+		let char=x.charAt(i);
+		if(charCount[char]>0)
+		{
+			charCount[char]++;
+		}
+		else
+		{
+			charCount[char]=1;
+		}
+	}
+for(let i=0;i<x.length;i++)
+	{
+		let current=x.charAt(i);
+		if(charCount[current]==1)
+		{
+			res=current;
+		}
+	}
+alert(res);
+
